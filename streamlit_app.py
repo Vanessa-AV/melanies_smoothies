b0073c0e1b9b9d8afef9083b5a2435c7b1cdf950
Vanessa-AV <vanessa.victoriano@us.abb.com>
2025-11-05 11:28:23 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,7 +21,7 @@ ingredients_list = st.multiselect(
     , my_dataframe
     , max_selections=5
 )
-
+import requests
 if ingredients_list:
     ingredients_string = ''
 
@@ -45,7 +45,7 @@ if ingredients_list:
         
         st.success('Your Smoothie is ordered!', icon="✅")
 
-import requests
+#import requests
 #smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
 # st.text(smoothiefroot_response.json())
 #sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
