1071aad1c991273a10a3bdc12c8cab2c79618343
Vanessa-AV <vanessa.victoriano@us.abb.com>
2025-11-05 11:34:36 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,7 +28,7 @@ if ingredients_list:
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ''
         st.subheader(fruit_chosen + 'Nutrition Information')
-        smoothiefroot_response = request.get("https://my.smoothiefroot.com/api/fruit/{search_on}")
+        smoothiefroot_response = request.get("https://my.smoothiefroot.com/api/fruit/{}".format(search_on))
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
     #st.write(ingredients_string)
