e51a85e930f1f58a91dc110ac6bd05d1ddfde741
Vanessa-AV <vanessa.victoriano@us.abb.com>
2025-11-05 11:51:00 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
+import requests
 
 # Write directly to the app
 st.title(f":cup_with_straw: Customize Your Smoothie:cup_with_straw:")
@@ -27,7 +28,6 @@ if ingredients_list:
     ingredients_string = ''
 
     for fruit_chosen in ingredients_list:
-        import requests
         ingredients_string += fruit_chosen + ''
         st.subheader(fruit_chosen + 'Nutrition Information')
         smoothiefroot_response = request.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
