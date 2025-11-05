86056514460f593d2a23b3774cc9e078ace62392
Vanessa-AV <vanessa.victoriano@us.abb.com>
2025-11-05 11:47:22 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,7 +16,7 @@ session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 # st.dataframe(data=my_dataframe, use_container_width=True)
 
-fruit_list = [row['FRUIT_NAME'] for row in my_dataframe.collect()]
+# fruit_list = [row['FRUIT_NAME'] for row in my_dataframe.collect()]
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredients:'
     , my_dataframe
@@ -29,7 +29,7 @@ if ingredients_list:
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ''
         st.subheader(fruit_chosen + 'Nutrition Information')
-        smoothiefroot_response = request.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
+        smoothiefroot_response = request.get("https://my.smoothiefruit.com/api/fruit/" + fruit_chosen)
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
     #st.write(ingredients_string)
